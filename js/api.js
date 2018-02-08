e2785e292f5581de16a48aacb9859baa1c31070c
Griffin Yates <gyates@fullsail.edu>
2018-02-07 23:05:15 -0500

    Added API functionality & modified some ID's into classes. Also added responsive CSS.

--- js/api.js
+++ js/api.js
@@ -4,6 +4,7 @@
 const form = document.querySelector("form");
 const input = document.querySelector("form > input");
 const cover = document.querySelectorAll('article > img');
+const artistImage = document.querySelectorAll('.cover');
 const album = document.querySelectorAll('article > h3');
 const plays = document.querySelectorAll('article > h5');
 const results = document.querySelector('#results');
@@ -11,27 +12,33 @@ let value = input.value;
 let content = '';
 
 
+function saveArtist(mbid) {
+  console.log("oops", mbid);
+}
+
+
 function buildHTML(data) {
   const {
     artist
   } = data.results.artistmatches;
-  content += `<h1>Results for ${value}</h1>`
+
   artist.forEach(artist => {
     // build a single artist template
+    console.log(artist.mbid);
+    const id = artist.mbid
     content +=
-      `<article>
-
-        <img id="cover" src="${
-          artist.image[3]["#text"] ? artist.image[3]["#text"] : 'images/placeholder.png' }" alt="Album Cover">
-        <h3 id="artist">${artist.name}</h3>
-        <h5 id="plays">Plays: ${artist.listeners}</h5>
+      `<article onclick="saveArtist(${id})" >
+        <img class="cover" src="${
+          artist.image[3]["#text"] ? artist.image[3]["#text"] : 'images/placeholder.png' }" alt="Album Cover"/>
+        <aside>
+          <h3 class="artist">${artist.name}</h4>
+          <h4 class="plays">Plays: ${artist.listeners}</h5>
+        </aside>
        </article>`;
 
     console.log(artist.image[2]["#text"]);
     cover.src = artist.image[2];
   })
-
-  content += `<button>View more on last.fm</button>`;
 
   results.innerHTML = content;
 }
@@ -43,7 +50,7 @@ form.addEventListener("submit", e => {
   value = input.value;
   console.log(value);
   const apikey = "375f63aa03654e88f0e2141ce6a9afb1";
-  const endpoint = `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${value}&api_key=${apikey}&format=json`;
+  const endpoint = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${value}&api_key=${apikey}&format=json`;
   fetch(endpoint)
     .then(response => {
       if (response.ok) {
@@ -56,3 +63,25 @@ form.addEventListener("submit", e => {
     .then(data => buildHTML(data))
     .catch(error => console.log('error is', error));
 })
+
+
+const button = document.querySelector('.closed');
+const ul = document.querySelector('nav > ul');
+const search = document.querySelector('#search');
+const logo = document.querySelector('#logo');
+const nav = document.querySelector('nav');
+
+button.addEventListener('click', (e) => {
+  ul.classList.toggle("open");
+  search.classList.toggle("hidden");
+  logo.classList.toggle("hidden");
+  nav.classList.toggle("gray");
+  console.log('clicked');
+})
+
+
+// const data = {
+//   username: "Emily",
+//   colors: ["red", "green", "blue", "purple"],
+//   id: 1
+// }
---
+++ script-test.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('nav');
+const toggleButton = document.querySelector('button');
+
+toggleButton.addEventListener('click', (e)=> {
+  
+})
